85a393f0572b208ea9fd47da8fd5b0a5222de235
dawsonhammdev <hammsdesigns@gmail.com>
2020-03-25 20:29:41 -0500

    I have completed the MVP

--- user-boarding/src/Form.js
+++ user-boarding/src/Form.js
@@ -2,25 +2,41 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import * as yup from "yup";
 
+const formSchema = yup.object().shape({
+  name: yup.string().required("Name is a required field."),
+  email: yup
+    .string()
+    .email("Must be a valid email address.")
+    .required("Must include email address."),
+  terms: yup.boolean().oneOf([true], "please agree to terms of use"),
+  positions: yup.string(),
+  motivation: yup.string().required("must include why you'd like to join")
+});
+
 export default function Form() {
+  // state for whether our button should be disabled or not.
+  const [buttonDisabled, setButtonDisabled] = useState(true);
 
-    const [formState, setFormState] = useState({
-        name: "",
-        email: "",
-        terms: "",
-        positions: "",
-        motivation: ""
-      });
+  // managing state for our form inputs
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    terms: "",
+    positions: "",
+    motivation: ""
+  });
 
-      const [errors, setErrors] = useState({
-        name: "",
-        email: "",
-        terms: "",
-        positions: "",
-        motivation: ""
-      });
+  // state for our errors
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    terms: "",
+    positions: "",
+    motivation: ""
+  });
 
-      const [post, setPost] = useState([]);
+  // new state to set our post request too. So we can console.log and see it.
+  const [post, setPost] = useState([]);
 
   useEffect(() => {
     formSchema.isValid(formState).then(valid => {
@@ -79,47 +95,62 @@ export default function Form() {
     setFormState(newFormData);
   };
 
-    <form >
-    <label htmlFor="name">
-      Name
-      <input
-        type="text"
-        name="name"
-      />
-    </label>
-    <label htmlFor="email">
-      Email
-      <input
-        type="text"
-        name="email"
-      />
-    </label>
-    <label htmlFor="motivation">
-      Why would you like to help?
-      <textarea
-        name="motivation"
-      />
-    </label>
-    <label htmlFor="positions">
-      What would you like to help with?
-      <select id="positions" name="positions">
-        <option value="Newsletter">Newsletter</option>
-        <option value="Yard Work">Yard Work</option>
-        <option value="Administrative Work">Administrative Work</option>
-        <option value="Tabling">Tabling</option>
-      </select>
-    </label>
-    <label htmlFor="terms" className="terms">
-      <input
-        type="checkbox"
-        name="terms"
-        checked={true}
-      />
-      Terms & Conditions
-    </label>
-    <button>Submit</button>
-  </form>
-
-  
-
+  return (
+    <form onSubmit={formSubmit}>
+      <label htmlFor="name">
+        Name
+        <input
+          type="text"
+          name="name"
+          value={formState.name}
+          onChange={inputChange}
+        />
+        {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
+      </label>
+      <label htmlFor="email">
+        Email
+        <input
+          type="text"
+          name="email"
+          value={formState.email}
+          onChange={inputChange}
+        />
+        {errors.email.length > 0 ? (
+          <p className="error">{errors.email}</p>
+        ) : null}
+      </label>
+      <label htmlFor="motivation">
+        Why would you like to help?
+        <textarea
+          name="motivation"
+          value={formState.motivation}
+          onChange={inputChange}
+        />
+        {errors.motivation.length > 0 ? (
+          <p className="error">{errors.motivation}</p>
+        ) : null}
+      </label>
+      <label htmlFor="positions">
+        What would you like to help with?
+        <select id="positions" name="positions" onChange={inputChange}>
+          <option value="Newsletter">Newsletter</option>
+          <option value="Yard Work">Yard Work</option>
+          <option value="Administrative Work">Administrative Work</option>
+          <option value="Tabling">Tabling</option>
+        </select>
+      </label>
+      <label htmlFor="terms" className="terms">
+        <input
+          type="checkbox"
+          name="terms"
+          checked={formState.terms}
+          onChange={inputChange}
+        />
+        Terms & Conditions
+      </label>
+      {/* displaying our post request data */}
+      <pre>{JSON.stringify(post, null, 2)}</pre>
+      <button disabled={buttonDisabled}>Submit</button>
+    </form>
+  );
 }
