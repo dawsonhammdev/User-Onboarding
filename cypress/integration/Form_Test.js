b914755834ea860f04a15e83f2828c618205d530
dawsonhammdev <hammsdesigns@gmail.com>
2020-03-26 14:36:26 -0500

    I added the dropdown and it has passed

--- cypress/integration/Form_Test.js
+++ cypress/integration/Form_Test.js
@@ -1,4 +1,4 @@
-describe("Testing our volunteer form", function() {
+describe("Testing our onboarding form", function() {
     beforeEach(function() {
       cy.visit("http://localhost:3000/");
     });
@@ -10,6 +10,12 @@ describe("Testing our volunteer form", function() {
         cy.get('input[name="email"]')
         .type("email@email.com")
         .should("have.value", "email@email.com")
+        cy.get('textarea')
+      .type('I am being onboarded')
+      .should("have.value", "I am being onboarded")
+      cy.get('#positions')
+      .select('Tabling')
+      .should("have.value", "Tabling")
 
 
 
