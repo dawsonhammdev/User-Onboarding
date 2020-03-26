d4dd776d09b13da07367d3544f7c0d717d4226f2
dawsonhammdev <hammsdesigns@gmail.com>
2020-03-26 14:23:35 -0500

    I have gotten it to write Dawson

--- cypress/integration/Form_Test.js
+++ cypress/integration/Form_Test.js
@@ -2,4 +2,18 @@ describe("Testing our volunteer form", function() {
     beforeEach(function() {
       cy.visit("http://localhost:3000/");
     });
+
+    it("Add test to inputs and submit form", function() {
+        cy.get('input[name="name"]')
+        .type("Dawson")
+        .should("have.value", "Dawson")
+
+
+
+
+
+
+
+
+    })
 })
