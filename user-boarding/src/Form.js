925858b0a5618513e8489c4ad9e9055d6069bcf7
dawsonhammdev <hammsdesigns@gmail.com>
2020-03-25 20:27:53 -0500

    I have added the yup

--- user-boarding/src/Form.js
+++ user-boarding/src/Form.js
@@ -47,6 +47,38 @@ export default function Form() {
       .catch(err => console.log(err.response));
   };
 
+  const validateChange = e => {
+    // Reach will allow us to "reach" into the schema and test only one part.
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.name === "terms" ? e.target.checked : e.target.value)
+
+      .then(valid => {
+        setErrors({
+          ...errors,
+          [e.target.name]: ""
+        });
+      })
+      .catch(err => {
+        setErrors({
+          ...errors,
+          [e.target.name]: err.errors[0]
+        });
+      });
+  };
+
+  const inputChange = e => {
+    e.persist();
+    const newFormData = {
+      ...formState,
+      [e.target.name]:
+        e.target.type === "checkbox" ? e.target.checked : e.target.value
+    };
+
+    validateChange(e);
+    setFormState(newFormData);
+  };
+
     <form >
     <label htmlFor="name">
       Name
